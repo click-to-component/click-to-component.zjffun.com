77c8880f5e33708a2d5f49017972da57a5080afd
Jufeng Zhang <zjffun@gmail.com>
2025-03-30 17:03:38 +0800

    feat: open next

---
+++ open-next.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// default open-next.config.ts file created by @opennextjs/cloudflare
+
+// @ts-nocheck
+import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
+
+export default defineCloudflareConfig({
+  incrementalCache: kvIncrementalCache,
+});
